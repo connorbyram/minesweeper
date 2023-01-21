efebb9bd1bd14ac0b6e33914774b23ddaa595ff9
connorbyram <connorabyram@gmail.com>
2023-01-20 16:45:38 -0800

    initial commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/*----- constants -----*/
+
+
+/*----- app's state (variables) -----*/
+let 
+let result; //if player choice is empty or bomb
+
+/*----- cached element references -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+
