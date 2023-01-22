d872fc15a8a338660ab9dfaf9a2b707a05211fc5
connorbyram <connorabyram@gmail.com>
2023-01-22 09:29:12 -0800

    started init & render functions

--- js/main.js
+++ js/main.js
@@ -9,10 +9,38 @@ let win;
 /*----- cached element references -----*/
 
 
+/*----- classes -----*/
+class Pedal {
+    constructor() {}
+
+   //is distortion?
+    //if no:
+        //if adj === 0 reveal & flood
+        //if adj
+}
+
 /*----- event listeners -----*/
 
 
 /*----- functions -----*/
+init();
+
+function init() {
+    board = []; //randomize distortion-pedal placement, calculate adjacent square #'s, identify 'empty' squares
+    win = nill;
+    render();
+}
+
+function render() {
+    renderBoard(); //iterate through squares
+    renderFlagCount();
+    renderTimer();
+
+}
+
+function renderBoard() {}
+function renderFlagCount() {}
+function renderTimer() {}
 
 // board   
 //     is mine?
