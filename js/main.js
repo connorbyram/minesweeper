3e47df0591b0d100441691b4587af576ad527a34
connorbyram <connorabyram@gmail.com>
2023-01-26 19:06:30 -0800

    increased mine count

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /*----- constants -----*/
 const BOARD_ROWS = 16;
 const BOARD_COLS = 16;
-let MINE_COUNT = 3;
+let MINE_COUNT = 40;
 ;
 const hidden = 'assets/images/empty.jpg'
 const flag = 'assets/images/flag.jpg'
